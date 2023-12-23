3cfdbf9194cb0c094676553f4cea448b98dbcbe2
Vid Smole <vid.smole@gmail.com>
2023-12-23 16:07:47 +0100

    Fix endpoint

--- app.py
+++ app.py
@@ -239,12 +239,12 @@ type_defs = load_schema_from_path("schema.graphql")
 schema = make_executable_schema(type_defs, query, snake_case_fallback_resolvers)
 
 
-@app.route("/graphql", methods=["GET"])
+@app.route("/data/graphql", methods=["GET"])
 def graphql_playground():
     return PLAYGROUND_HTML, 200
 
 
-@app.route("/graphql", methods=["POST"])
+@app.route("/data/graphql", methods=["POST"])
 def graphql_server():
     data = request.get_json()
     success, result = graphql_sync(schema, data, context_value=request, debug=app.debug)
